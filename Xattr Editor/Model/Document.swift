439aeda79bb67354742246aa5e9ce4aeb0a63d40
Emilio P Egido <eaperez@me.com>
2026-03-01 22:05:10 +0100

    Add Document NSDocument subclass and bump version
    - Add a minimal NSDocument subclass (Xattr Editor/Model/Document.swift) that overrides autosavesInPlace to true to suppress autosave warnings
    - Bump version to 3.0.1 (33).

--- Xattr Editor/Model/Document.swift
+++ Xattr Editor/Model/Document.swift
@@ -5,6 +5,8 @@
 
 import AppKit
 
+// Minimal NSDocument subclass overriding autosavesInPlace
+// to fix autosavesInPlace warnings in console
 class Document: NSDocument {
     override class var autosavesInPlace: Bool {
         return true
